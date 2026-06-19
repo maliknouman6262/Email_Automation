124e2e85ddd64c6cacc237906a876fbebf7688f0
Muhammad Nouman <maliknouman6262@gmail.com>
2026-06-19 19:23:40 +0500

    updated api baseURL to live railway backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,7 +97,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'Shivamshukla913152@gmail.com'
-EMAIL_HOST_PASSWORD ="fsqokcxdqcmkjgfw"
+EMAIL_HOST_PASSWORD ="wpuetdmhpefdhkxr"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Database
